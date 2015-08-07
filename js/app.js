465affb267bb56cd56facd2acbd4185a4847f995
Justin Williams <justinwilliams42@gmail.com>
2015-08-07 20:35:06 +0100

    add result to feedLoad callback; add argument to skip rendering

--- js/app.js
+++ js/app.js
@@ -6,59 +6,59 @@
  * jQuery.
  */
 
-(function () {
-
-  'use strict';
-
-  // The names and URLs to all of the feeds we'd like available.
-  var allFeeds = [
-    {
-      name: 'Udacity Blog',
-      url: 'http://blog.udacity.com/feeds/posts/default?alt=rss'
-    }, {
-      name: 'CSS Tricks',
-      url: 'http://css-tricks.com/feed'
-    }, {
-      name: 'HTML5 Rocks',
-      url: 'http://feeds.feedburner.com/html5rocks'
-    }, {
-      name: 'Linear Digressions',
-      url: 'http://feeds.feedburner.com/udacity-linear-digressions'
-    }
-  ];
-
-  /* This function starts up our application. The Google Feed
-   * Reader API is loaded asynchonously and will then call this
-   * function when the API is loaded.
-   */
-  function init() {
-    // Load the first feed we've defined (index of 0).
-    loadFeed(0);
+// The names and URLs to all of the feeds we'd like available.
+var allFeeds = [
+  {
+    name: 'Udacity Blog',
+    url: 'http://blog.udacity.com/feeds/posts/default?alt=rss'
+  }, {
+    name: 'CSS Tricks',
+    url: 'http://css-tricks.com/feed'
+  }, {
+    name: 'HTML5 Rocks',
+    url: 'http://feeds.feedburner.com/html5rocks'
+  }, {
+    name: 'Linear Digressions',
+    url: 'http://feeds.feedburner.com/udacity-linear-digressions'
   }
+];
 
-  /* This function performs everything necessary to load a
-   * feed using the Google Feed Reader API. It will then
-   * perform all of the DOM operations required to display
-   * feed entries on the page. Feeds are referenced by their
-   * index position within the allFeeds array.
-   * This function all supports a callback as the second parameter
-   * which will be called after everything has run successfully.
+/* This function starts up our application. The Google Feed
+ * Reader API is loaded asynchonously and will then call this
+ * function when the API is loaded.
+ */
+function init() {
+  // Load the first feed we've defined (index of 0).
+  loadFeed(0);
+}
+
+/* This function performs everything necessary to load a
+ * feed using the Google Feed Reader API. It will then
+ * perform all of the DOM operations required to display
+ * feed entries on the page. Feeds are referenced by their
+ * index position within the allFeeds array.
+ * This function all supports a callback as the second parameter
+ * which will be called after everything has run successfully.
+ */
+// NOTE: added an additional argument here to skip rendering
+// this is for the test which checks that all feeds load
+function loadFeed(id, cb, skipRender) {
+  var feedUrl  = allFeeds[id].url,
+      feedName = allFeeds[id].name,
+      feed     = new google.feeds.Feed(feedUrl);
+
+  /* Load the feed using the Google Feed Reader API.
+   * Once the feed has been loaded, the callback function
+   * is executed.
    */
-  function loadFeed(id, cb) {
-    var feedUrl  = allFeeds[id].url,
-        feedName = allFeeds[id].name,
-        feed     = new google.feeds.Feed(feedUrl);
-
-    /* Load the feed using the Google Feed Reader API.
-     * Once the feed has been loaded, the callback function
-     * is executed.
-     */
-    feed.load(function (result) {
-      if (!result.error) {
-        /* If loading the feed did not result in an error,
-         * get started making the DOM manipulations required
-         * to display the feed entries on screen.
-         */
+  feed.load(function (result) {
+    if (!result.error) {
+      /* If loading the feed did not result in an error,
+       * get started making the DOM manipulations required
+       * to display the feed entries on screen.
+       */
+      // if skipRender is falsy then go ahead and render the output
+      if (!skipRender) {
         var container     = $('.feed'),
             title         = $('.header-title'),
             entries       = result.feed.entries,
@@ -76,61 +76,60 @@
           container.append(entryTemplate(entry));
         });
       }
+    }
 
-      if (cb) {
-        cb();
-      }
-    });
-  }
+    // NOTE: added returning the result via the callback
+    // so we can check for errors
+    if (cb) {
+      cb(result);
+    }
+  });
+}
 
-  /* Google API: Loads the Feed Reader API and defines what function
-   * to call when the Feed Reader API is done loading.
+/* Google API: Loads the Feed Reader API and defines what function
+ * to call when the Feed Reader API is done loading.
+ */
+google.load('feeds', '1');
+google.setOnLoadCallback(init);
+
+/* All of this functionality is heavily reliant upon the DOM, so we
+ * place our code in the $() function to ensure it doesn't execute
+ * until the DOM is ready.
+ */
+$(function () {
+  var feedList         = $('.feed-list'),
+      feedItemTemplate = Handlebars.compile($('.tpl-feed-list-item').html()),
+      feedId           = 0,
+      menuIcon         = $('.menu-icon-link');
+
+  /* Loop through all of our feeds, assigning an id property to
+   * each of the feeds based upon its index within the array.
+   * Then parse that feed against the feedItemTemplate (created
+   * above using Handlebars) and append it to the list of all
+   * available feeds within the menu.
    */
-  google.load('feeds', '1');
-  google.setOnLoadCallback(init);
+  allFeeds.forEach(function (feed) {
+    feed.id = feedId;
+    feedList.append(feedItemTemplate(feed));
+    feedId++;
+  });
 
-  /* All of this functionality is heavily reliant upon the DOM, so we
-   * place our code in the $() function to ensure it doesn't execute
-   * until the DOM is ready.
+  /* When a link in our feedList is clicked on, we want to hide
+   * the menu, load the feed, and prevent the default action
+   * (following the link) from occuring.
    */
-  $(function () {
-    var feedList         = $('.feed-list'),
-        feedItemTemplate = Handlebars.compile($('.tpl-feed-list-item').html()),
-        feedId           = 0,
-        menuIcon         = $('.menu-icon-link');
+  feedList.on('click', 'a', function () {
+    var item = $(this);
 
-    /* Loop through all of our feeds, assigning an id property to
-     * each of the feeds based upon its index within the array.
-     * Then parse that feed against the feedItemTemplate (created
-     * above using Handlebars) and append it to the list of all
-     * available feeds within the menu.
-     */
-    allFeeds.forEach(function (feed) {
-      feed.id = feedId;
-      feedList.append(feedItemTemplate(feed));
+    $('body').addClass('menu-hidden');
+    loadFeed(item.data('id'));
+    return false;
+  });
 
-      feedId++;
-    });
-
-    /* When a link in our feedList is clicked on, we want to hide
-     * the menu, load the feed, and prevent the default action
-     * (following the link) from occuring.
-     */
-    feedList.on('click', 'a', function () {
-      var item = $(this);
-
-      $('body').addClass('menu-hidden');
-      loadFeed(item.data('id'));
-      return false;
-    });
-
-    /* When the menu icon is clicked on, we need to toggle a class
-     * on the body to perform the hiding/showing of our menu.
-     */
-    menuIcon.on('click', function () {
-      $('body').toggleClass('menu-hidden');
-    });
-
-  }());
-
-})();
+  /* When the menu icon is clicked on, we need to toggle a class
+   * on the body to perform the hiding/showing of our menu.
+   */
+  menuIcon.on('click', function () {
+    $('body').toggleClass('menu-hidden');
+  });
+}());
